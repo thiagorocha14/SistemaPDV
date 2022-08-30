92af49b463c8a14cc615de2e21d0f57e04e4295d
Thiago Rocha <thiagormnb@gmail.com>
2022-08-29 21:42:30 -0300

    ta adicionando item

---
+++ src/service/ItemVendaService.java
@@ -0,0 +1,13 @@
+package service;
+
+import javax.ejb.Stateless;
+
+import modelo.ItemVenda;
+
+@Stateless
+public class ItemVendaService extends GenericService<ItemVenda> {
+
+	public ItemVendaService() {
+		super(ItemVenda.class);
+	}
+}
